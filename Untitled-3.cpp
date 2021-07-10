3be7a08c472a4059911350c9cf1c6af0853c0eac
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2021-07-10 18:02:30 +0530

    Add files via upload

---
+++ Untitled-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    
+    int x,y;
+    int product;
+    cout<<"type a number:";
+    cin>>x;
+    cout<<"type another number:";
+    cin>>y;
+    product=x*y;
+    
+    cout<<"your product is:"<<product;
+    return 0;
+}
